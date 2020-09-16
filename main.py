7a0d797cab64a378c03619ea1556b8977ae9b1b1
SamuelAug1 <mlksaam@gmail.com>
2020-09-16 23:48:47 +0000

    .

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import os
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/')
+@app.route('/unifran')
 def index():
-  return '<h2>Universidade de Franca</h2>'
+ return '<h2>Universidade de Franca!</h2>'
 
-if __name__ == '__name__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host= '0.0.0.0', port=port)
+if __name__ == "__main__":
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
